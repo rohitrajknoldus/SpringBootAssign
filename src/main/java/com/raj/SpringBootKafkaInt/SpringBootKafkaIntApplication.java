b8324b152d927d15937444c36dbb13d8d1252305
Rohit RAj <rohit.raj@knoldus.com>
2021-11-25 12:48:37 +0530

    first commit

---
+++ src/main/java/com/raj/SpringBootKafkaInt/SpringBootKafkaIntApplication.java
@@ -0,0 +1,13 @@
+package com.raj.SpringBootKafkaInt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKafkaIntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootKafkaIntApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raj/SpringBootKafkaInt/SpringBootKafkaIntApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raj.SpringBootKafkaInt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKafkaIntApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
